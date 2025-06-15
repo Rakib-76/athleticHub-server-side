e7dd849169c2db4c8ec0fecc53c79a6abab9b0b6
Rakib-76 <mrrakibhasan76@gmail.com>
2025-06-16 00:36:24 +0600

    added the forth commit to update and delete page

--- index.js
+++ index.js
@@ -42,12 +42,36 @@ async function run() {
     });
 
 
+
+
     app.post('/events', async (req, res) => {
       const newEvent = req.body;
       console.log(newEvent);
       const result = await eventsCollection.insertOne(newEvent);
       res.send(result);
     });
+
+    app.delete('/events/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await eventsCollection.deleteOne(query);
+      res.send(result)
+    });
+
+
+    app.put('/events/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedEvent = req.body;
+      const updatedDoc = {
+        $set: updatedEvent
+
+      }
+      const result = await eventsCollection.updateOne(filter, updatedDoc, options);
+      res.send(result);
+
+    })
 
 
     // Connect the client to the server	(optional starting in v4.7)
