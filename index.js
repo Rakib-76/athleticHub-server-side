f95431717b38ff32561270863fdbd60798eb5600
Rakib-76 <mrrakibhasan76@gmail.com>
2025-06-17 12:14:20 +0600

    added the correction to querry email

--- index.js
+++ index.js
@@ -112,11 +112,11 @@ async function run() {
       const result = await eventsCollection.updateOne(filter, updatedDoc, options);
       res.send(result);
 
-    })
+    });
 
 
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
