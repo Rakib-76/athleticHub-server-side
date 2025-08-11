8139ed24253231d159410b6575c281a89691621d
Rakib-76 <mrrakibhasan76@gmail.com>
2025-08-11 16:32:35 +0600

    added the get operation for details page

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function run() {
       res.send(events);
     });
 
-
+    // here it is used for details page 
     app.get('/events/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
